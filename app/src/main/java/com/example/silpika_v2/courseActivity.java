687c1cb3cabdb81a14abd9111da8814725266c11
PintuBeast <abhishek.jk1992@gmail.com>
2020-07-19 14:59:50 +0530

    Commit 1

---
+++ app/src/main/java/com/example/silpika_v2/courseActivity.java
@@ -0,0 +1,14 @@
+package com.example.silpika_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class courseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
